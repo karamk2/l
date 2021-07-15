414f8278279ddb9e3e12ffc973fe88ffa4412b8b
karamk2 <82733051+karamk2@users.noreply.github.com>
2021-07-16 00:51:11 +0300

    Update bot.js

--- config/bot.js
+++ config/bot.js
@@ -2,7 +2,7 @@ module.exports = {
    
 
     discord: {
-        token: 'ODU1MzY2NjE2OTM3Mzk4Mjky.YMxcPQ.jN3lQI2B12SeVVrVwWOXuAcSpC4',
+        token: '',
         prefix: '$',
         activity: 'الله يوخذك اذا بتستخدم غيري ',
     },
