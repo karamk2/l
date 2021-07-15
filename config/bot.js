e3964376bc191f631388aa183b45d3db9a20b2e6
karamk2 <82733051+karamk2@users.noreply.github.com>
2021-07-16 00:50:01 +0300

    Add files via upload

---
+++ config/bot.js
@@ -0,0 +1,11 @@
+module.exports = {
+   
+
+    discord: {
+        token: 'ODU1MzY2NjE2OTM3Mzk4Mjky.YMxcPQ.jN3lQI2B12SeVVrVwWOXuAcSpC4',
+        prefix: '$',
+        activity: 'الله يوخذك اذا بتستخدم غيري ',
+    },
+
+    filters: ['8D', 'gate', 'haas', 'phaser', 'treble', 'tremolo', 'vibrato', 'reverse', 'karaoke', 'flanger', 'mcompand', 'pulsator', 'subboost', 'bassboost', 'vaporwave', 'nightcore', 'normalizer', 'surrounding'],
+};
